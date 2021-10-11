90a0cdda861d4fa28d025b8f211a33e61f85949e
Romain Fayat <r.fayat@gmail.com>
2021-10-11 18:06:13 +0200

    Add PWM chunks

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 Launch PWM on an arduino via a serial port using python
 
 ## Requirements -- Arduino
-Install the [Arduino PWM Frequency Library](https://code.google.com/archive/p/arduino-pwm-frequency-library/downloads) from your arduino IDE. 
+Install the [Arduino PWM Frequency Library](https://code.google.com/archive/p/arduino-pwm-frequency-library/downloads) from your arduino IDE.
+
+## Setting up the hardware
+
+**Warning:**
+All pins do not support interruptions (e.g. [only pins 2 and 3 on the arduino Uno](https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/)), set `PIN_COUNTER` to a pin that can support interruptions.
--- pwm_arduino/pwm_arduino.ino
+++ pwm_arduino/pwm_arduino.ino
@@ -6,12 +6,18 @@
 #define PIN_PWM 9
 #define PIN_RUNNING 8
 #define PIN_ENABLE 10
+#define PIN_COUNTER 2
 bool running = false;
 byte duty_cycle_converted;
 int32_t FREQUENCY = 10;
+const int CHUNK_SIZE = 50;
+volatile bool detected_pulse = false;
+int counter = 0;
+int interruption_time = 1000; // ms
 
 void run_pwm(float duty_cycle){
   /* Run PWM on a given pin at an input frequency and duty cycle.*/
+  running = true;
   duty_cycle_converted = 255 * duty_cycle;
   analogWrite(PIN_PWM, duty_cycle_converted);
 }
@@ -20,10 +26,17 @@ void stop_pwm(){
   /* Stop pwm on the pwm pin.*/
   //digitalWrite(PIN_PWM, LOW);
   digitalWrite(PIN_PWM, 0);
+  counter = 0;
+  running = false;
 }
 
+void callback_input_counter(){
+  detected_pulse = true;
+  Serial.print("tartiflette ");
+}
 
 void setup() {
+  Serial.begin(9600);
   // PWM Pin
   InitTimersSafe();
   bool success = SetPinFrequencySafe(PIN_PWM, FREQUENCY);
@@ -32,22 +45,31 @@ void setup() {
   pinMode(PIN_RUNNING, OUTPUT);
   // Enable pin
   pinMode(PIN_ENABLE, INPUT_PULLUP);
+  // Counter pin
+  pinMode(PIN_COUNTER, INPUT);
+  attachInterrupt(digitalPinToInterrupt(PIN_COUNTER), callback_input_counter, FALLING);
   // Stop pwm if it was running
   stop_pwm();
 }
 
 void loop() {
+  if (detected_pulse){
+    counter += 1;
+    detected_pulse = false;
+  }
+  if (counter >= CHUNK_SIZE){
+    stop_pwm();
+    delay(interruption_time);
+  }
 
   if (digitalRead(PIN_ENABLE)){
     if (!running){
       // Run PWM
-      running = true;
       run_pwm(.5);
     }
   }
   else {
     stop_pwm();
-    running = false;
   }
   digitalWrite(PIN_RUNNING, running);
   delay(1);
